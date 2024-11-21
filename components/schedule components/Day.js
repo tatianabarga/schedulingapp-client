4904dbfb3c92ba61a507e509ee24d92c083ebc2e
Tatiana Barga <tatibspersonal@gmail.com>
2024-11-21 14:53:16 -0600

    rough layout of day component JSX

---
+++ components/schedule components/Day.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Day() {
+  return (
+    <div className="Day Component">
+      <div className="weekday-header">Title</div> {/* make this display weekday property from object */}
+      <div className="task-cont">
+        {/* map through tasks */}
+      </div>
+    </div>
+  );
+}
