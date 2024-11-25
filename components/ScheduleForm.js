03cbd8616e256f1ad9798b874c66733a84b4dc38
Tatiana Barga <tatibspersonal@gmail.com>
2024-11-24 18:07:18 -0600

    beginning of schedule form

---
+++ components/ScheduleForm.js
@@ -0,0 +1,20 @@
+// import React, { useState } from 'react';
+
+export default function ScheduleForm() {
+  // const [dates, setDates] = useState([]);
+
+  // const initialState = {
+
+  // }
+
+  // const onSubmit = () => {
+  //   const datesArr = scheduleObj.dates.split(', ');
+  //   setDates(datesArr); // something like this but in reverse. dates will come in as an array and we need to make it into a string
+  // };
+
+  return (
+    <div>
+      hi
+    </div>
+  );
+}
