a4ff20ef5f62ca4e9b03e960deb64cd9277d3898
Tatiana Barga <tatibspersonal@gmail.com>
2024-11-25 23:36:45 -0600

    more goals stuff

---
+++ components/scheduleComponents/Goals.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types';
+import React, { useEffect, useState } from 'react';
+
+export default function Goals({ goals }) {
+  const [goalsArr, setGoalsArr] = useState([]);
+
+  useEffect(() => {
+    setGoalsArr(goals.split('--'));
+  }, [goals]);
+
+  return (
+    <div>
+      <div className="goals-header">Goals</div>
+      <div className="goal-items-cont">
+        {goalsArr.map((goal) => (
+          <div className="goal-item">
+            <div className="goal-text">{goal}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+Goals.propTypes = {
+  goals: PropTypes.string.isRequired,
+};
--- components/scheduleComponents/Schedule.js
+++ components/scheduleComponents/Schedule.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import Day from './Day';
 import { getDaysBySchedule } from '../../utils/data/dayData';
+import Goals from './Goals';
 
 export default function Schedule({ scheduleObj }) {
   // const [dates, setDates] = useState([]);
@@ -23,14 +24,28 @@ export default function Schedule({ scheduleObj }) {
     <div className="schedule-component">
       <div className="schedule-label">{scheduleObj?.label}</div>
       <div className="task-area">
-        {/* tasks component here */}
+        {/* <div className="tasks">
+          <Tasks />
+        </div> */}
         {/* map through days in this week in scheduleObj and pass to Day component */}
-        <div className="lists">
+        <div className="days">
           {daysArr.map((day) => (
             <Day key={day.id} dayObj={day} />
           ))}
         </div>
       </div>
+      <div className="goals-area">
+        <div className="add-goal-btn">
+          <button type="button">Add a Goal</button> {/* TODO: make this click to open new goal input */}
+        </div>
+        {scheduleObj.goals
+          ? (
+            <div className="goals">
+              <Goals goals={scheduleObj.goals} />
+            </div>
+          ) : null}
+      </div>
+
     </div>
   );
 }
@@ -41,6 +56,6 @@ Schedule.propTypes = {
     label: PropTypes.string,
     user: PropTypes.number,
     dates: PropTypes.string,
+    goals: PropTypes.string,
   }).isRequired,
-  // onUpdate: PropTypes.func.isRequired,
 };
