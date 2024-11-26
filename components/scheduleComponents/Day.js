d879a879d0559752fda8f5a62c3f84d1cdb588f1
Tatiana Barga <tatibspersonal@gmail.com>
2024-11-26 00:19:10 -0600

    creat new task functions for days and staging started

--- components/scheduleComponents/Day.js
+++ components/scheduleComponents/Day.js
@@ -5,6 +5,15 @@ import Task from './Task';
 
 export default function Day({ dayObj }) {
   const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState(false);
+
+  const changeTaskInput = () => {
+    if (taskInput === true) {
+      setTaskInput(false);
+    } else {
+      setTaskInput(true);
+    }
+  };
 
   useEffect(() => {
     if (dayObj.id) {
@@ -16,6 +25,12 @@ export default function Day({ dayObj }) {
     <div className="Day Component">
       <div className="weekday-header">{dayObj?.weekday}</div> {/* make this display weekday property from object */}
       <div className="day-date">{dayObj?.date}</div>
+      <div className="add-task-btn">
+        <button type="button" onClick={changeTaskInput}> Add a Task</button>
+      </div>
+      {taskInput ? (
+        <div className="task-input">task input</div> /* TODO: make this new task input */
+      ) : null}
       <div className="task-cont">
         {/* map through tasks */}
         {tasks.map((task) => (
--- components/scheduleComponents/Tasks.js
+++ components/scheduleComponents/Tasks.js
@@ -3,16 +3,33 @@ import { getTasksBySchedule } from '../../utils/data/tasksData';
 
 export default function Tasks(scheduleId) {
   const [tasks, setTasks] = useState([]);
+  const [taskInput, setTaskInput] = useState(false);
+
+  const changeTaskInput = () => {
+    if (taskInput === true) {
+      setTaskInput(false);
+    } else {
+      setTaskInput(true);
+    }
+  };
 
   useEffect(() => {
     getTasksBySchedule(scheduleId).then(setTasks((data) => data.filter((item) => item.day_id === 0)));
   }, [scheduleId]);
 
   return (
-    <div className="staged-tasks">
-      {tasks.map((task) => (
-        <div className="task">{task}</div>
-      ))}
+    <div className="tasks-component">
+      <div className="add-task-btn">
+        <button type="button" onClick={changeTaskInput}> Add a Task</button>
+      </div>
+      {taskInput ? (
+        <div className="task-input">task input</div> /* TODO: make this new task input */
+      ) : null}
+      <div className="staged-tasks">
+        {tasks.map((task) => (
+          <div className="task">{task}</div>
+        ))}
+      </div>
     </div>
   );
 }
