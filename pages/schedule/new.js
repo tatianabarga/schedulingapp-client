2e95e4dd4cc3b09679403991cddde58f590620ff
Tatiana Barga <tatibspersonal@gmail.com>
2024-12-02 13:36:09 -0600

    add schedule btn added, routes to new schedule page, pulls up sched form

---
+++ pages/schedule/new.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ScheduleForm from '../../components/ScheduleForm';
+
+export default function newSchedule() {
+  return (
+    <div>
+      <ScheduleForm />
+    </div>
+  );
+}
