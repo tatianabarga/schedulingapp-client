0345b16a4a2f82a2a68a45e15b79a59df7702039
Tatiana Barga <tatibspersonal@gmail.com>
2024-12-02 13:51:04 -0600

    submit button added and works but user needs to be set

--- components/ScheduleForm.js
+++ components/ScheduleForm.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { createSchedule } from '../utils/data/scheduleData';
 
 export default function ScheduleForm() {
-  // const [dates, setDates] = useState([]);
-  const [payload, setPayload] = useState({
+  const initialState = {
     label: '',
-    dates: '',
-  });
+    // dates: '',
+  };
 
-  // const initialState = {
+  // const [dates, setDates] = useState([]);
+  const [payload, setPayload] = useState({ initialState });
 
-  // }
+  useEffect(() => {
+    console.log(payload);
+  }, [payload]);
 
-  // const onSubmit = () => {
-  //   const datesArr = scheduleObj.dates.split(', '); // move this to onChangeDates func
-  //   setDates(datesArr); // something like this but in reverse. dates will come in as an array and we need to make it into a string
-  // };
+  const onSubmit = () => {
+    // const datesArr = scheduleObj.dates.split(', '); // move this to onChangeDates func
+    // setDates(datesArr); // something like this but in reverse. dates will come in as an array and we need to make it into a string
+    if (payload === initialState) {
+      alert('Schedule cannot be empty');
+      return;
+    }
+    createSchedule(payload);
+  };
 
   return (
     <div className="schedule-form-component">
@@ -26,7 +34,9 @@ export default function ScheduleForm() {
         value={payload.label}
         onChange={(e) => setPayload({ ...payload, label: e.target.value })}
       />
-
+      <div className="submit-btn">
+        <button type="button" onClick={onSubmit}>Submit</button>
+      </div>
       {/* choose a tempate */}
       {/* choose a date */}
     </div>
